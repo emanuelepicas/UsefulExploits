aef1b4561cebe9df42c813dd7492a90db8e566d8
Emanuele Picariello <emanuelepicariello97@gmail.com>
2022-07-12 18:28:46 +0200

    updated solution for lab WebSecurity Academy

---
+++ Java/Serialization/data/productcatalog/ProductTemplate.java
@@ -0,0 +1,27 @@
+package data.productcatalog;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ProductTemplate implements Serializable
+{
+    static final long serialVersionUID = 1L;
+
+    private final String id;
+    private transient Product product;
+
+    public ProductTemplate(String id)
+    {
+        this.id = id;
+    }
+    public String getId()
+    {
+        return id;
+    }
+
+}
